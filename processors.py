018da5819606e8ee1dee8f92421e256f5129ec4c
Heinrich Kruger <heindsight@gmail.com>
2018-06-07 21:02:13 +0100

    More gradations

--- processors.py
+++ processors.py
@@ -1,4 +1,23 @@
-THRESHOLD = 128
+THRESHOLDS = {
+    36: ' ',
+    72: '.',
+    108: 'o',
+    144: 'O',
+    180: '0',
+    216: 'X',
+    255: '#',
+}
+
+CONVERSION_ARRAY = THRESHOLDS.keys()
+
+
+def float_to_character(f):
+    lower_t = 0
+    for higher_t in CONVERSION_ARRAY:
+        if higher_t > f >= lower_t:
+            return THRESHOLDS[higher_t]
+        lower_t = higher_t
+    return THRESHOLDS[higher_t]
 
 
 def process_tuple(rgb):
@@ -14,41 +33,47 @@ def process_square(square):
 
 
 def threshold(value):
-    return '#' if value < THRESHOLD else ' '
+    return '#' if value < THRESHOLDS else ' '
 
 
 def convert(square):
-    return threshold(
-        process_square(square)
-    )
+    return [
+        [
+            float_to_character(process_tuple(pixel))
+            for pixel in row
+        ]
+        for row in square
+    ]
 
 
-white_leena = [
-    [(255, 255, 255), (255, 255, 255), (255, 255, 255)],
-    [(255, 255, 255), (255, 255, 255), (255, 255, 255)],
-    [(255, 255, 255), (255, 255, 255), (255, 255, 255)],
-    [(255, 255, 255), (255, 255, 255), (255, 255, 255)],
-]
+#  white_leena = [
+#      [(255, 255, 255), (255, 255, 255), (255, 255, 255)],
+#      [(255, 255, 255), (255, 255, 255), (255, 255, 255)],
+#      [(255, 255, 255), (255, 255, 255), (255, 255, 255)],
+#      [(255, 255, 255), (255, 255, 255), (255, 255, 255)],
+#  ]
 
-black_leena = [
-    [(0, 0, 0), (0, 0, 0), (0, 0, 0)],
-    [(0, 0, 0), (0, 0, 0), (0, 0, 0)],
-    [(0, 0, 0), (0, 0, 0), (0, 0, 0)],
-    [(0, 0, 0), (0, 0, 0), (0, 0, 0)],
-]
-
-
-half_leena = [
-    [(0, 0, 0), (0, 0, 0), (0, 0, 0)],
-    [(0, 0, 0), (0, 0, 0), (0, 0, 0)],
-    [(255, 255, 255), (255, 255, 255), (255, 255, 255)],
-    [(255, 255, 255), (255, 255, 255), (255, 255, 255)],
-]
+#  black_leena = [
+#      [(0, 0, 0), (0, 0, 0), (0, 0, 0)],
+#      [(0, 0, 0), (0, 0, 0), (0, 0, 0)],
+#      [(0, 0, 0), (0, 0, 0), (0, 0, 0)],
+#      [(0, 0, 0), (0, 0, 0), (0, 0, 0)],
+#  ]
 
 
-assert process_square(half_leena) == 127
-assert process_square(black_leena) == 0
-assert process_square(white_leena) == 255
-assert convert(half_leena) == '#'
-assert convert(white_leena) == ' '
-assert convert(black_leena) == '#'
+#  half_leena = [
+#      [(0, 0, 0), (0, 0, 0), (0, 0, 0)],
+#      [(0, 0, 0), (0, 0, 0), (0, 0, 0)],
+#      [(255, 255, 255), (255, 255, 255), (255, 255, 255)],
+#      [(255, 255, 255), (255, 255, 255), (255, 255, 255)],
+#  ]
+
+
+#  assert process_square(half_leena) == 127
+#  assert process_square(black_leena) == 0
+#  assert process_square(white_leena) == 255
+#  assert convert(half_leena) == '#'
+#  assert convert(white_leena) == ' '
+#  assert convert(black_leena) == '#'
+
+#  assert float_to_character
